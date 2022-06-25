f0403ef2496f281a7d8adec122e75fdfd07f129a
gowtha123 <gowthamsurya91@gmail.com>
2022-06-25 16:58:29 +0530

    first time

---
+++ src/test/java/gitclass/Classsample.java
@@ -0,0 +1,13 @@
+package gitclass;
+
+public class Classsample {
+public void method1() {
+	System.out.println("1");
+}
+public void method2() {
+	System.out.println("1");
+}
+public void method3() {
+	System.out.println("1");
+}
+}
